b0748cb9d0d76bfb88dc9d8e51f28bc550b10707
Pooja Shrivastava <poo1234.kumari@gmail.com>
2022-05-29 18:45:04 +0530

    UI progress bar

---
+++ script.js
@@ -0,0 +1,57 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circle = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+next.addEventListener('click', ()=> {
+    currentActive++
+     
+    if(currentActive > circle.length) {
+        currentActive = circle.length
+    }
+    update()
+    //console.log(currentActive);
+    console.log(currentActive);
+})
+
+prev.addEventListener('click', ()=> {
+    currentActive--
+     
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+
+    update()
+    //console.log(currentActive);
+    console.log(currentActive);
+})
+
+function update() {
+    circle.forEach((circle, idx) => {
+        if(idx < currentActive) {
+            circle.classList.add('active')
+        }
+        else {
+            circle.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')
+ 
+    progress.style.width = (actives.length-1) / (circle.length-1) * 100 + '%'
+
+    if(currentActive === 1){
+        prev.disabled = true
+    }
+    else if(currentActive == circle.length){
+        next.disabled = true
+    }
+    else{
+        prev.disabled = false
+        next.disabled =  false
+    }
+
+    console.log(actives.length, circle.length)
+}
